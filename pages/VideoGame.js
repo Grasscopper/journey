9294ba35d6c36a034b2321f16baab08f3345bd35
Justin Stransky <dva@colostate.edu>
2022-08-18 21:17:49 -0400

    Game is no longer a page; hopefully deployment works

--- pages/VideoGame.js
+++
@@ -1,8 +0,0 @@
-import React from 'react'
-import { Client } from 'boardgame.io/react'
-import { Game } from './Game'
-import Board from './Board'
-
-const VideoGame = Client({ game: Game, board: Board, numPlayers: 1, debug: false })
-
-export default VideoGame
