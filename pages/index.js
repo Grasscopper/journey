276953d2522eddaac6df12259b944e1aa8b3b0b0
Justin Stransky <dva@colostate.edu>
2022-08-18 21:42:57 -0400

    Everything that isn't a page has been moved; except index.js and _app.js

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import { Client } from 'boardgame.io/react'
 import { Game } from '../Game'
-import Board from './Board'
+import Board from '../Board'
 
 const VideoGame = Client({ game: Game, board: Board, numPlayers: 1, debug: false })
 
